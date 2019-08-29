446add71e4f93cf242af0f1df7ac4822f731f2b7
messi.chaib <messi.chaib@USABOS1003103L.ad.infosys.com>
2019-08-29 14:30:12 -0700

    Added two controllers, AppController, SignRegisterController, a repository and a service class

--- Messi_Batch46_Project/src/main/java/com/messi/controller/AppController.java
+++ Messi_Batch46_Project/src/main/java/com/messi/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.messi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class AppController {
 	
 	@GetMapping("/")
 	public String display() {
+		return "messi";
+	}
+	@PostMapping("/")
+	public String insertRecord() {
+		
 		return "messi";
 	}
 
